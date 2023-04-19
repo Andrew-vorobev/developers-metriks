8e913fe37cc702f863a933fad4aff2d5574736d6
Danila Petrov <dp200308@gmail.com>
2023-04-19 21:41:59 +0500

    Add login form

---
+++ src/app/modules/auth/login-preview/login-preview.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { GitlabService } from '../../../stats/gitlab.service';
+import { GitlabAuthService } from '../gitlab-auth.service';
+import { UserExtraDto } from '../../../stats/dto/userExtra.dto';
+
+@Component({
+  selector: 'app-login-preview',
+  templateUrl: './login-preview.component.html',
+  styleUrls: ['./login-preview.component.css'],
+})
+export class LoginPreviewComponent implements OnChanges {
+  protected isLoadingNow = true;
+  @Input() public isLoggedIn = false;
+  protected userData:
+    | Pick<UserExtraDto, 'name' | 'username' | 'email' | 'avatar_url'>
+    | undefined;
+  constructor(
+    protected gitlabAuthService: GitlabAuthService,
+    protected gitlabService: GitlabService
+  ) {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.isLoadingNow = true;
+    if (this.gitlabAuthService.hasValidAccessToken()) {
+      this.isLoggedIn = true;
+      this.gitlabService.getProfileData().subscribe(data => {
+        this.userData = data;
+        this.isLoadingNow = false;
+      });
+    } else {
+      this.isLoadingNow = false;
+    }
+  }
+}
