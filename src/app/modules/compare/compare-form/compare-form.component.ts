1fd3fdbec4b861b6d366f032eeafccdccd8ff375
PsevdoKot <youkos00@mail.ru>
2023-04-22 22:02:44 +0500

    Compare form init

---
+++ src/app/modules/compare/compare-form/compare-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-compare-form',
+  templateUrl: './compare-form.component.html',
+  styleUrls: ['./compare-form.component.css']
+})
+export class CompareFormComponent {
+
+}
