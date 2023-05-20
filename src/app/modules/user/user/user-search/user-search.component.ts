68714190814bfa59a94245ad929bb4307aa80360
PsevdoKot <youkos00@mail.ru>
2023-05-20 22:17:08 +0500

    User search added on main and user page

---
+++ src/app/modules/user/user/user-search/user-search.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-user-search',
+  templateUrl: './user-search.component.html',
+  styleUrls: ['./user-search.component.css'],
+})
+export class UserSearchComponent implements OnInit {
+  protected form!: FormGroup;
+  constructor() {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      search: new FormControl<string>('', Validators.required),
+    });
+  }
+
+  protected submit(): void {}
+}
