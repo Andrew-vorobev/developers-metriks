493d83e3b670be7bb436473536e67fa489b92a98
PsevdoKot <youkos00@mail.ru>
2023-06-03 11:42:28 +0500

    Added projects and commits on user page

---
+++ src/app/modules/user/user-data/user-data.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-data',
+  templateUrl: './user-data.component.html',
+  styleUrls: ['./user-data.component.css'],
+})
+export class UserDataComponent {
+  @Input() public userService: UserService;
+}
